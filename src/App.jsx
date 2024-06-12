cb5de5f4e6b7c5ee1343def6e2f79b27a92ce5e6
RomaBez <bezvuglyak.roman@gmail.com>
2024-06-12 21:15:49 +0300

    first trying

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+const alertStyles = {
+  margin: 8,
+  padding: "12px 16px",
+  borderRadius: 4,
+  backgroundColor: "gray",
+  color: "white",
+};
+
+export const App = () => {
+  return (
+    <>
+      <p style={alertStyles}>Please update your email!</p>
+      <p style={alertStyles}>There was an error during transaction!</p>
+      <p style={alertStyles}>Payment received, thank you for your purchase!</p>
+    </>
+  );
+};
+
+export default App 
